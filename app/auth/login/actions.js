2656485d5491d99d8c1425c12126325c7066886e
ll33t3r <xx11337312@gmail.com>
2026-01-16 20:12:51 -0500

    V2 Stable: Fixed Login Paths and UTF8 Encoding

---
+++ app/auth/login/actions.js
@@ -0,0 +1,24 @@
+﻿'use server'
+
+// CHANGED: Pointing to 'lib' instead of 'utils'
+import { createClient } from '../../../lib/supabase/server'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
+
+export async function login(formData) {
+  const supabase = createClient()
+  
+  const data = {
+    email: formData.get('email'),
+    password: formData.get('password'),
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    return { error: error.message }
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
